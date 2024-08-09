cb8c2da91da8cf3451c7368a6566400931cdd62a
isaiahSAIT <DatuIsaiahMadVisales.Visales@edu.sait.ca>
2024-08-09 17:08:52 -0600

    Update contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,7 +2,7 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 document.getElementById('submit-button').addEventListener('click', function(event) {
-    event.preventDefault();
+    event.preventDefault(); //Remember this cancels event
 
     const contactPage = document.getElementById('contact-page');
     contactPage.innerHTML = '<p style="font-size: 24px;">Thank you for your message</p>';
